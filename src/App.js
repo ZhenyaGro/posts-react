59f08d57b65f015fe3fa73e58279c37e653e5d1e
ZhenyaGro <zhenyagro@icloud.com>
2025-01-20 18:10:03 +0300

    App auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './components/AppRouter';
 import { Navbar } from './components/UI/navbar/Navbar';
+import { AuthContext } from './context';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setLoading] = useEffect(true);
+
+  useEffect(() => {
+    if (localStorage.getItem('auth')) setIsAuth(true);
+    setLoading(false);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRouter />
-    </BrowserRouter>
+    <AuthContext.Provider value={{ isAuth, setIsAuth, isLoading }}>
+      <BrowserRouter>
+        <Navbar />
+        <AppRouter />
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
