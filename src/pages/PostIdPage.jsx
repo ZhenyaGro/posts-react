a184548413da5cbebcfe7e9b014ac4386a1e6df2
ZhenyaGro <zhenyagro@icloud.com>
2025-01-20 18:09:05 +0300

    Post view page

---
+++ src/pages/PostIdPage.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useFetching } from '../hooks/useFetching';
+import PostService from '../API/PostService';
+import { Loader } from '../components/UI/Loader/Loader';
+
+export const PostIdPage = () => {
+  const params = useParams();
+  const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
+
+  const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+    const response = await PostService.getById(id);
+    setPost(response.data);
+  });
+  const [fetchComments, isCommentsLoading, commentsError] = useFetching(
+    async (id) => {
+      const response = await PostService.getCommentsById(id);
+      setComments(response.data);
+    }
+  );
+
+  useEffect(() => {
+    fetchPostById(params.id);
+    fetchComments(params.id);
+  }, []);
+
+  return (
+    <div>
+      Страница поста ID = {params.id}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          {post.id}. {post.title}
+        </div>
+      )}
+      <h1>Комментарии</h1>
+      {isCommentsLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          {comments.map((comment) => (
+            <div key={comment.id} style={{ marginTop: '15px' }}>
+              <h5>{comment.email}</h5>
+              <div>{comment.body}</div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
